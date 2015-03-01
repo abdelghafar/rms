71b3c5d3e8f9689e818b981947b77b0d7e924c39
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-03-01 09:36:05 +0300

    Update index.php

---
+++ test.php
@@ -0,0 +1,18 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <script src="js/jquery-1.11.0.min.js" type="text/javascript"></script>
+        <script src="js/jqwidgets/jqwidgets/jqxbuttons.js" type="text/javascript"></script>
+        <link href="js/jqwidgets/jqwidgets/styles/jqx.base.css" rel="stylesheet" type="text/css"/>
+        <script type="text/javascript">
+
+        </script>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
