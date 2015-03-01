9610d453350153fa29e02dbafba113499ee62be7
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-03-01 10:49:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,63 @@
-<?php
+<?
+session_start();
+require_once './redirect.php';
+Redirect();
+require_once 'lib/Smarty/libs/Smarty.class.php';
+$smarty = new Smarty();
 
-echo 'asdassad';
+$smarty->assign('style_css', 'style.css');
+$smarty->assign('style_responsive_css', 'style.responsive.css');
+$smarty->assign('jquery_js', 'jquery.js');
+$smarty->assign('script_js', 'script.js');
+$smarty->assign('script_responsive_js', 'script.responsive.js');
+$smarty->assign('index_php', 'index.php');
+$smarty->assign('Researchers_register_php', 'Researchers/register.php');
+$smarty->assign('login_php', 'login.php');
+$smarty->assign('fqa_php', 'fqa.php');
+$smarty->assign('contactus_php', 'contactus.php');
+
+$smarty->display('templates/header.tpl');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+
+
+    </head>
+    <body>
+        <div class="art-content-layout layout-item-1">
+            <div class="art-content-layout-row">
+                <div class="art-layout-cell layout-item-2" style="width: 25%" >
+                    <h3 style="text-align: right;">شروط التقديم</h3>
+                    <p><img width="64" height="64" alt="" src="images/41_64x64.png" style="float:left;margin-right:10px;" class=""></p><p style="text-align: justify;">
+                        شروط التقدم لتمويل المشروعات البحثية والعناصر المطلوب إستيفائها</p><p style="text-align: left;"><a href="subscribe_rules.php" target="_self"><span style="font-weight: bold; color: rgb(245, 37, 10); ">...التفاصيل</span></a></p>
+                    <p>
+                    </p>
+                </div><div class="art-layout-cell layout-item-3" style="width: 25%" >
+                    <h3 style="text-align: right;">إجراءات تمويل المشاريع البحثية</h3>
+                    <p><img width="64" height="64" alt="" src="images/48_64x64.png" style="float:left;margin-right:10px;" class=""></p>
+                    <p style="text-align: justify;">خطوات ومراحل تمويل المشروع البحثيى&nbsp;من مرحلة التقديم حتى مرحلة إتمام تنفيذ المشروع البحثى.</p>
+                    <p style="text-align: left;"><span style="text-align: left;"><a href="fund_reg.php" target="_self" style="text-decoration: underline; color: rgb(219, 157, 0);"><span style="font-weight: bold; color: rgb(245, 37, 10); ">...التفاصيل</span></a></span><a href="#" style="text-decoration: underline; color: rgb(255, 207, 87); cursor: pointer;"></a><a href="#"></a></p>
+                </div><div class="art-layout-cell layout-item-4" style="width: 25%" >
+                    <h3 style="text-align: right;">لائحة البحث العلمى السعودية</h3><p><img width="64" height="64" alt="" src="images/15_64x64.png" style="float:left;margin-right:10px;" class=""></p>
+                    <p style="text-align: justify;">كل ما يتعلق بالقواعد واللوائح &nbsp;المنظمة لعملية البحث العلمى بالجامعات السعودية.<br></p>
+                    <p style="text-align: left;"><span style="text-align: left;"><a href="fund_reg.php" target="_self" style="text-decoration: underline; color: rgb(219, 157, 0);"><span style="font-weight: bold; color: rgb(245, 37, 10); ">...التفاصيل</span></a></span><a href="#" style="text-decoration: underline; color: rgb(255, 207, 87); cursor: pointer;"></a><a href="#"></a></p>
+                </div><div class="art-layout-cell layout-item-3" style="width: 25%" >
+                    <h3 style="text-align: right;">النماذج والإستمارات</h3>
+                    <p><img width="64" height="64" alt="" src="images/34_64x64.png" style="float:left;margin-right:10px;" class=""></p>
+                    <p style="text-align: justify;">النماذج المستخدمة فى عملية تمويل المشروعات البحثية للكل من الباحثين والمحكمين.</p>
+                    <p style="text-align: left;"><span style="text-align: left;"><a href="subscribe_rules.php" target="_self" style="text-decoration: underline; color: rgb(219, 157, 0);"><span style="font-weight: bold; color: rgb(245, 37, 10);">...التفاصيل</span></a></span><a href="#" style="text-decoration: underline; color: rgb(255, 207, 87);"></a><a href="#"></a></p>
+                </div>
+            </div>
+        </div>
+
+
+    </body>
+</html>
+
+
+<?
+$smarty->display('../templates/footer.tpl');
 ?>
---
+++ profile.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="en-US"><head><!-- Created by Artisteer v4.1.0.59861 -->
+    <meta charset="utf-8">
+    <title>New Page</title>
+    <meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0, user-scalable = no, width = device-width">
+
+    <!--[if lt IE 9]><script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link rel="stylesheet" href="style.css" media="screen">
+    <!--[if lte IE 7]><link rel="stylesheet" href="style.ie7.css" media="screen" /><![endif]-->
+    <link rel="stylesheet" href="style.responsive.css" media="all">
+
+    
+    <script src="jquery.js"></script>
+    <script src="script.js"></script>
+    <script src="script.responsive.js"></script>
+
+
+<style>.art-content .art-postcontent-0 .layout-item-0 { padding: 0px;  }
+.art-content .art-postcontent-0 .layout-item-1 { color: #292929; background: #F2F2F2;  }
+.ie7 .art-post .art-layout-cell {border:none !important; padding:0 !important; }
+.ie6 .art-post .art-layout-cell {border:none !important; padding:0 !important; }
+
+</style></head>
+<body>
+<div id="art-main">
+<nav class="art-nav">
+    <div class="art-nav-inner">
+    <ul class="art-hmenu"><li><a href="index.php" class="">الصفحة الرئيسية</a></li><li><a href="register.php" class="">تسجيل جديد</a></li><li><a href="login.php" class="">الدخول</a></li><li><a href="fqa.php" class="">الإستفسارات</a></li><li><a href="contactus.php" class="">بيانات الإتصال</a></li></ul> 
+        </div>
+    </nav>
+<div class="art-sheet clearfix">
+<header class="art-header">
+<div class="art-slider art-slidecontainerheader" data-width="1000" data-height="380">
+    <div class="art-slider-inner">
+<div class="art-slide-item art-slideheader0">
+
+</div>
+<div class="art-slide-item art-slideheader1">
+<div class="art-textblock art-slideheader1-object832388275" data-left="0%">
+        <div class="art-slideheader1-object832388275-text-container">
+        <div class="art-slideheader1-object832388275-text"><p style="color: #FFF3D6; font-size:40px;font-family:'Century Gothic', Arial, 'Arial Unicode MS', Helvetica, Sans-Serif;font-weight:bold;font-style:normal;text-decoration:none">الإبتــــــكار</p></div>
+    </div>
+    
+</div>
+</div>
+<div class="art-slide-item art-slideheader2">
+<div class="art-textblock art-slideheader2-object1285826474" data-left="0%">
+        <div class="art-slideheader2-object1285826474-text-container">
+        <div class="art-slideheader2-object1285826474-text"><p style="color: #FFF3D6; font-size:40px;font-family:'Century Gothic', Arial, 'Arial Unicode MS', Helvetica, Sans-Serif;font-weight:bold;font-style:normal;text-decoration:none">الإبــــداع</p></div>
+    </div>
+    
+</div>
+</div>
+<div class="art-slide-item art-slideheader3">
+<div class="art-textblock art-slideheader3-object1450141196" data-left="0%">
+        <div class="art-slideheader3-object1450141196-text-container">
+        <div class="art-slideheader3-object1450141196-text"><p style="color: #FFF3D6; font-size:40px;font-family:'Century Gothic', Arial, 'Arial Unicode MS', Helvetica, Sans-Serif;font-weight:bold;font-style:normal;text-decoration:none">العمل الجماعى</p></div>
+    </div>
+    
+</div>
+</div>
+<div class="art-slide-item art-slideheader4">
+<div class="art-textblock art-slideheader4-object1709499836" data-left="0%">
+        <div class="art-slideheader4-object1709499836-text-container">
+        <div class="art-slideheader4-object1709499836-text"><p style="color: #E3ECF2; font-size:40px;font-family:'Century Gothic', Arial, 'Arial Unicode MS', Helvetica, Sans-Serif;font-weight:bold;font-style:normal;text-decoration:none">خدمة المجتمع</p></div>
+    </div>
+    
+</div>
+</div>
+
+    </div>
+</div>
+<div class="art-slidenavigator art-slidenavigatorheader" data-left="92.1">
+<a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a>
+</div>
+
+
+
+    <div class="art-shapes">
+
+            </div>
+
+
+
+
+                
+                    
+</header>
+<div class="art-layout-wrapper">
+                <div class="art-content-layout">
+                    <div class="art-content-layout-row">
+                        <div class="art-layout-cell art-content"><article class="art-post art-article">
+                                
+                                <div class="art-postcontent art-postcontent-0 clearfix"><div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 20%" >
+        <p>menu<br/></p>
+    </div><div class="art-layout-cell layout-item-1" style="width: 80%" >
+        <p><br /></p>
+    </div>
+    </div>
+</div>
+</div>
+
+
+</article></div>
+                    </div>
+                </div>
+            </div><footer class="art-footer">
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 100%">
+        <p><span style="font-weight: bold;"><span style="text-shadow: rgb(0, 0, 0) 0px 0px 14px;">جميع الحقوق محفوظة&nbsp;</span><span style="text-shadow: rgb(0, 0, 0) 0px 0px 14px;">© معهد البحوث العلمية وإحياء التراث الإسلامى - جامعة أم القرى</span></span></p>
+    </div>
+    </div>
+</div>
+
+</footer>
+
+    </div>
+</div>
+
+
+</body></html>
