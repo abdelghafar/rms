28cbf4cc343231d1a9725fdfc1d95cad7062b320
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-03-10 12:23:38 +0300

    Update index.php

--- Researchers/inc/uploadIntro.inc.php
+++ Researchers/inc/uploadIntro.inc.php
@@ -13,11 +13,18 @@ $arabicAbs = $obj->GetAbstract_ar_url($projectId);
 $engAbs = $obj->GetAbstract_en_url($projectId);
 $into = $obj->GetIntro_url($projectId);
 $review = $obj->GetLitReview_url($projectId);
+$research_method = $obj->GetResearch_method_url($projectId);
+$value = $obj->GetValueToKingdomUrl($projectId);
+$refs = $obj->GetRefsUrl($projectId);
+
 
 $arabicAbs_file = 0;
 $engAbs_file = 0;
 $into_file = 0;
 $review_file = 0;
+$research_method_ok = 0;
+$value_ok = 0;
+$ref_ok = 0;
 $msg = "";
 if (strlen($arabicAbs) != 0) {
     $arabicAbs_file = 1;
@@ -39,7 +46,27 @@ if (strlen($review) != 0) {
 } else {
     $msg.= "<li>" . "من فضلك قم بتحميل المسح الأدبي" . "</li>";
 }
-if ($arabicAbs_file == 1 && $engAbs_file == 1 && $into_file == 1 && $review_file == 1) {
+
+if (strlen($research_method) != 0) {
+    $research_method_ok = 1;
+} else {
+    $msg.= "<li>" . "من فضلك قم بتحميل منهجية البحث" . "</li>";
+}
+
+if (strlen($value) != 0) {
+    $value_ok = 1;
+} else {
+    $msg.= "<li>" . "من فضلك قم بتحميل القيمة للدولة" . "</li>";
+}
+
+if (strlen($refs) != 0) {
+    $ref_ok = 1;
+} else {
+    $msg.= "<li>" . "من فضلك قم بتحميل المراجع" . "</li>";
+}
+
+
+if ($arabicAbs_file == 1 && $engAbs_file == 1 && $into_file == 1 && $review_file == 1 && $research_method_ok == 1 && $value_ok == 1 && $ref_ok == 1) {
 
     echo '<script>' . ' window.location.assign("project_stuff.php?q=' . $projectId . '");' . '</script>';
 } else {
