5a21b2c777a5ae0a9640798e5ea7b214582f2ba7
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-03-16 20:10:34 +0300

    Update index.php

--- Researchers/inc/fileUpload.php
+++ Researchers/inc/fileUpload.php
@@ -10,8 +10,7 @@ if (isset($_GET['q'])) {
     $file_name = "uploads/" . $prefix . "/";
 
     $target_file = $target_dir . sha1(urlencode(pathinfo($_FILES['fileToUpload']['name'], PATHINFO_FILENAME))) . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
-
-    $file_name .= sha1(urlencode(pathinfo($_FILES['fileToUpload']['name'], PATHINFO_FILENAME))) . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+    //$file_name .= sha1(urlencode(pathinfo($_FILES['fileToUpload']['name'], PATHINFO_FILENAME))) . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
 
     if ($_GET['type'] == 'coAuthor_agreement') {
         $person_id = $_GET['person_id'];
@@ -27,6 +26,61 @@ if (isset($_GET['q'])) {
         $person_id = $_GET['person_id'];
         $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     }
+
+    $type = filter_input(INPUT_GET, 'type');
+    switch ($type) {
+        case 'arAbsUpload': {
+                $file_name .= 'summary_ar' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                $target_file = $target_dir . 'summary_ar' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                break;
+            }
+        case 'enAbsUpload': {
+                $file_name .= 'summary_en' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                $target_file = $target_dir . 'summary_en' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                break;
+            }
+        case 'introUpload': {
+                $file_name .= 'introduction' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                $target_file = $target_dir . 'introduction' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                break;
+            }
+        case 'reviewUpload': {
+                $file_name .= 'review' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                $target_file = $target_dir . 'review' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                break;
+            }
+        case 'research_method': {
+                $file_name .= 'research_method' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                $target_file = $target_dir . 'research_method' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                break;
+            }
+        case 'objective_approach': {
+                $file_name .= 'objective_approach' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                $target_file = $target_dir . 'objective_approach' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                break;
+            }
+        case 'objective_tasks': {
+                $file_name .= 'objective_tasks' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                $target_file = $target_dir . 'objective_tasks' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                break;
+            }
+        case 'working_plan': {
+                $file_name .= 'working_plan' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                $target_file = $target_dir . 'working_plan' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                break;
+            }
+        case 'value_to_kingdom': {
+                $file_name .= 'value_to_kingdom' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                $target_file = $target_dir . 'value_to_kingdom' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                break;
+            }
+        case 'refs': {
+                $file_name .= 'refs' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                $target_file = $target_dir . 'refs' . '.' . pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+                break;
+            }
+    }
+
 
     $uploadOk = 1;
     $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
@@ -44,8 +98,9 @@ if (isset($_GET['q'])) {
     }
 // Check if file already exists
     if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
+        unlink($target_file);
+        //echo "Sorry, file already exists.";
+        //$uploadOk = 0;
     }
 // Check file size 2*1024*1024
     if ($_FILES["fileToUpload"]["size"] > 2 * 1024 * 1024) {
