741e4625f89e551d0a5c2f62fe277de647df3573
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-03-30 14:04:38 +0300

    reformat code

--- Researchers/ajax/setProgramSession.php
+++ Researchers/ajax/setProgramSession.php
@@ -8,24 +8,31 @@ if (isset($_GET['program'])) {
 
         $program_string = filter_input(INPUT_GET, 'program', FILTER_SANITIZE_STRING);
         switch ($program_string) {
-            case 'ba7th': {
-                    $program_id = $t->GetProgramId('ba7th');
-                    $_SESSION['program'] = $program_id;
-                    break;
-                }
-            case 'ra2d': {
-                    $program_id = $t->GetProgramId('ra2d');
-                    $_SESSION['program'] = $program_id;
-                    break;
-                }
-            case 'wa3da': {
-                    $program_id = $t->GetProgramId('wa3da');
-                    $_SESSION['program'] = $program_id;
-                    break;
-                }
-            default : {
-                    header("Location: ../index.php");
-                    break;
+            case 'ba7th':
+            {
+                $program_id = $t->GetProgramId('ba7th');
+                $_SESSION['program'] = $program_id;
+
+                break;
+            }
+            case 'ra2d':
+            {
+                $program_id = $t->GetProgramId('ra2d');
+                $_SESSION['program'] = $program_id;
+
+                break;
+            }
+            case 'wa3da':
+            {
+                $program_id = $t->GetProgramId('wa3da');
+                $_SESSION['program'] = $program_id;
+
+                break;
+            }
+            default :
+                {
+                header("Location: ../index.php");
+                break;
                 }
         }
     } else {
