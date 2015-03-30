75833f50a51d990b52bc505f06373a46c20a2dfc
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-03-30 14:04:57 +0300

    reformat code

--- Researchers/selectProgram.php
+++ Researchers/selectProgram.php
@@ -1,7 +1,7 @@
 <?
 session_start();
 if (trim($_SESSION['User_Id']) == 0 || !isset($_SESSION['User_Id'])) {
-    header('Location:../Login.php');
+    header('Location:../login.php');
 } else {
     $rule = $_SESSION['Rule'];
     if ($rule != 'Researcher') {
@@ -26,50 +26,57 @@ $smarty->assign('contactus_php', '../contactus.php');
 
 $smarty->display('../templates/Loggedin.tpl');
 ?>
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" href="css/reigster-layout.css"/> 
-    <script type="text/javascript" src="../js/jqwidgets/scripts/gettheme.js"></script> 
-    <script type="text/javascript" src="../js/jquery-ui/js/jquery-1.9.0.js"></script>
-    <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxcore.js"></script>
-    <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxinput.js"></script>
-    <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxdatetimeinput.js"></script>
-    <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxcalendar.js"></script>
-    <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxtooltip.js"></script>
-    <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxswitchbutton.js"></script>
-    <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxbuttons.js"></script>
-    <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxmaskedinput.js"></script>
-    <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxvalidator.js"></script>
-    <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxdropdownlist.js"></script>
-    <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxscrollbar.js"></script>
-    <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxnumberinput.js"></script>
-    <script src="../js/jqwidgets/jqwidgets/globalization/globalize.js" type="text/javascript"></script>
-    <link href="../js/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+    <head>
+        <meta charset="UTF-8">
+        <META HTTP-EQUIV="expires" CONTENT="0">
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="css/reigster-layout.css"/>
+        <script type="text/javascript" src="../js/jqwidgets/scripts/gettheme.js"></script>
+        <script type="text/javascript" src="../js/jquery-ui/js/jquery-1.9.0.js"></script>
+        <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxcore.js"></script>
+        <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxinput.js"></script>
+        <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxdatetimeinput.js"></script>
+        <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxcalendar.js"></script>
+        <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxtooltip.js"></script>
+        <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxswitchbutton.js"></script>
+        <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxbuttons.js"></script>
+        <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxmaskedinput.js"></script>
+        <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxvalidator.js"></script>
+        <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxdropdownlist.js"></script>
+        <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxscrollbar.js"></script>
+        <script type="text/javascript" src="../js/jqwidgets/jqwidgets/jqxnumberinput.js"></script>
+        <script src="../js/jqwidgets/jqwidgets/globalization/globalize.js" type="text/javascript"></script>
+        <link href="../js/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
 
-    <link rel="stylesheet" href="../js/jqwidgets/jqwidgets/styles/jqx.base.css" type="text/css" />
-    <link rel="stylesheet" href="../js/jqwidgets/jqwidgets/styles/jqx.energyblue.css" type="text/css"/> 
+        <link rel="stylesheet" href="../js/jqwidgets/jqwidgets/styles/jqx.base.css" type="text/css"/>
+        <link rel="stylesheet" href="../js/jqwidgets/jqwidgets/styles/jqx.energyblue.css" type="text/css"/>
 
-    <script src="../js/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-    <script type='text/javascript'>
-        $(document).ready(function () {
-            $('#wa3da').click(function () {
-                $.ajax({url: 'ajax/setProgramSession.php?program=wa3da'});
-                window.location.assign('Researchers_View.php');
+        <script src="../js/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script type='text/javascript'>
+            $(document).ready(function () {
+                $('#wa3da').click(function () {
+                    $.ajax({url: 'ajax/setProgramSession.php?program=wa3da', success: function (data, textStatus, jqXHR) {
+                        window.location.assign('Researchers_View.php');
+                    } });
+
+                });
+                $('#ba7th').click(function () {
+                    $.ajax({url: 'ajax/setProgramSession.php?program=ba7th', success: function (data, textStatus, jqXHR) {
+                        window.location.assign('Researchers_View.php');
+                    } });
+
+                });
+                $('#ra2d').click(function () {
+                    $.ajax({url: 'ajax/setProgramSession.php?program=ra2d', success: function (data, textStatus, jqXHR) {
+                        window.location.assign('Researchers_View.php');
+                    } });
+
+                });
             });
-            $('#ba7th').click(function () {
-                $.ajax({url: 'ajax/setProgramSession.php?program=ba7th'});
-                window.location.assign('Researchers_View.php');
-            });
-            $('#ra2d').click(function () {
-                $.ajax({url: 'ajax/setProgramSession.php?program=ra2d'});
-                window.location.assign('Researchers_View.php');
-            });
-        });
-    </script>
-</head>
-<body>
+        </script>
+    </head>
+    <body>
     <?php
     if ($_SESSION['gender'] == 0) {
         echo '<button id="ra2d" type="button" class="btn btn-success btn-lg" style="width: 300px; height: 100px;margin-left: 80px;">
@@ -91,6 +98,6 @@ $smarty->display('../templates/Loggedin.tpl');
     }
     ?>
 
-</body>
+    </body>
 <?
 $smarty->display('../templates/footer.tpl');
