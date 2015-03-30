2484a972b59a6d4ccfc3720dce7ca8b83c0f1e69
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-03-30 20:17:45 +0300

    reformat code

--- Researchers/project_stuff.php
+++ Researchers/project_stuff.php
@@ -15,8 +15,8 @@ require_once '../lib/Reseaches.php';
 
 require_once '../lib/users.php';
 
-if (isset($_GET['q'])) {
-    $projectId = $_GET['q'];
+if (isset($_SESSION['q'])) {
+    $projectId = $_SESSION['q'];
     $obj = new Reseaches();
     $UserId = $_SESSION['User_Id'];
     $u = new Users();
@@ -116,7 +116,7 @@ $personId = $users->GetPerosnId($userId, 'Researcher');
                         editable: false,
                         pageable: false,
                         filterable: true,
-                        width: 850,
+                        width: 880,
                         pagesize: 5,
                         autoheight: true,
                         columnsresize: true,
@@ -168,7 +168,7 @@ $personId = $users->GetPerosnId($userId, 'Researcher');
                         editable: false,
                         pageable: false,
                         filterable: true,
-                        width: 850,
+                        width: 880,
                         pagesize: 5,
                         autoheight: true,
                         columnsresize: true,
@@ -224,7 +224,7 @@ $personId = $users->GetPerosnId($userId, 'Researcher');
                         editable: false,
                         pageable: false,
                         filterable: true,
-                        width: 850,
+                        width: 880,
                         pagesize: 5,
                         autoheight: true,
                         columnsresize: true,
@@ -233,7 +233,7 @@ $personId = $users->GetPerosnId($userId, 'Researcher');
                         columns: [
                             {text: 'person_id', datafield: 'person_id', width: 3, align: 'center', cellsalign: 'center', hidden: true},
                             {text: 'Emplyoee Id / رقم المنسوب', dataField: 'empCode', width: 250, align: 'right', cellsalign: 'right'},
-                            {text: 'Name / الاسم', dataField: 'name_ar', width: 250, align: 'right', cellsalign: 'right'},
+                            {text: 'Name / الاسم', dataField: 'name_ar', width: 280, align: 'right', cellsalign: 'right'},
                             {text: 'Title / الدرجة العلمية', dataField: 'position', width: 200, align: 'right', cellsalign: 'right'},
                             {text: 'Specialization/ التخصص ', dataField: 'major_Field', width: 150, align: 'right', cellsalign: 'right'}
                         ]
@@ -290,50 +290,76 @@ $personId = $users->GetPerosnId($userId, 'Researcher');
             });
         });
 
+        function wizard_step(current_step) {
+            var cs = current_step;
+            for (var i = 1; i < cs; i++) {
+                $("#img_" + i).attr("src", "images/" + i + "_finished.png");
+                //$('#bar_' + i).css('backgroundImage', "url('images/finished.png')");
+            }
+            $("#img_" + cs).attr("src", "images/" + cs + "_current.png");
+            //$('#bar_' + cs).css('backgroundImage', "url('images/current.png')");
+            for (var i = cs + 1; i <= 9; i++) {
+                $("#img_" + i).attr("src", "images/" + i + "_unfinish.png");
+                //if (i < 9)
+                // $('#bar_' + i).css('backgroundImage', "url('images/unfinish.png')");
+            }
+        }
     </script>
 </head>
 <body>
-    <fieldset style="width: 95%;text-align: right;"> 
+<div>
+    <?
+    require_once 'wizard_steps.php';
+    ?>
+</div>
+<script type="text/javascript">
+    wizard_step(3);
+</script>
+
+<fieldset style="width: 95%;text-align: right;">
         <legend>
             <label>
                 الفريق البحثي / Research Team
             </label>
         </legend>
-        <div id='jqxWidget' style="font-size: 13px; font-family: Verdana; float: right;margin-top: 10px;margin-right:25px;margin-bottom: 30px;">
 
-            <span class="classic-font">الباحث الرئيس/ PI</span>
-            <hr/>
+    <div id='jqxWidget' style="font-size: 13px; font-family: Verdana; float: right;margin-top: 10px;margin-right:25px;margin-bottom: 30px;">
+
+        <h2 style="font-size: 14px">الباحث الرئيس/ PI</h2>
+        <hr/>
             <div id="gridPI"></div>
             <br/><br/>
-            <span class="classic-font">الباحثين المشاركين / CO-Is</span>
-            <hr/>
-            <input type="button" style="margin-bottom: 15px;" id='AddNewCoIs' value="Add / اضافة"/>
 
-            <div id='SearchFrm' style="width: 852px;height: auto;">
+        <h2 style="font-size: 14px">الباحثين المشاركين / CO-Is</h2>
+        <hr/>
+        <input type="button" style="margin-bottom: 15px;margin-right:730" id='AddNewCoIs' value="Add / اضافة"/>
+
+        <div id='SearchFrm' style="width: 852px;height: auto;">
 
             </div>
 
             <div id='gridCoI'></div>
             <br/><br/>
-            <span class="classic-font">أخري -Other Personal </span>
-            <hr/>
-            <input type="button" style="margin-bottom: 15px;" id='AddNewOtherPersonal' value="Add / اضافة"/>
-            <div id='SearchPersonalFrm' style="width: 852px;height: auto;"></div>
+
+        <h2 style="font-size: 14px">أخري -Other Personal </h2>
+        <hr/>
+        <input type="button" style="margin-bottom: 15px;margin-right:730 " id='AddNewOtherPersonal'
+               value="Add / اضافة"/>
+
+        <div id='SearchPersonalFrm' style="width: 852px;height: auto;"></div>
             <div id='gridOthers'></div>
         </div>
     </fieldset>
     <table style="width: 100%;">
         <tr>
             <td>
-                <a id="submit_button" href="phases.php?q=<? echo $projectId; ?>" style="float: right;margin-left: 25px;margin-top: 20px;">
-                    <img src="images/next.png" style="border: none;" alt="next"/>
-
+                <a href="uploadIntro.php" style="float: right;margin-left: 25px;margin-top: 20px;">
+                    <img src="images/back.png" style="border: none;" alt="back"/>
                 </a>
             </td>
             <td>
-                <a href="uploadIntro.php?q=<? echo $projectId; ?>" style="float: left;margin-left: 25px;margin-top: 20px;">
-                    <img src="images/back.png" style="border: none;" alt="back"/>
-
+                <a id="submit_button" href="phases.php" style="float: left;margin-left: 25px;margin-top: 20px;">
+                    <img src="images/next.png" style="border: none;" alt="next"/>
                 </a>
             </td>
         </tr>
