bb9173ada0adf4bc3c4b177c35d4e785551c5335
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-08 12:51:26 +0300

    reformat code

--- Data/GetStuff_Roles.php
+++ Data/GetStuff_Roles.php
@@ -10,7 +10,7 @@ require_once '../lib/config.php';
 require_once '../lib/mysqlConnection.php';
 
 $con = new MysqlConnect();
-$stmt = "SELECT`seq_id`, `role_name` ,`role_name_en`  FROM `stuff_roles` WHERE `seq_id`>2;";
+$stmt = "SELECT`seq_id`, `role_name` ,`role_name_en`  FROM `stuff_roles` WHERE seq_id > 2";
 $result = $con->ExecuteNonQuery($stmt);
 while ($row = mysql_fetch_array($result)) {
     $rs[] = array('seq_id' => $row['seq_id'], 'role_name' => $row['role_name_en'] . " / " . $row['role_name']);
