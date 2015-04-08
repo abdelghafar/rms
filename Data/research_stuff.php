00106ec5a658a78521dcebd5cc3120d68ebbd0ee
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-08 12:47:25 +0300

    reformat code

--- Data/research_stuff.php
+++ Data/research_stuff.php
@@ -8,16 +8,26 @@
 
 require_once '../lib/config.php';
 require_once '../lib/mysqlConnection.php';
-
+require_once '../lib/persons.php';
 
 $conn = new MysqlConnect();
+$person = new Persons();
 
 if (isset($_REQUEST['project_id'])) {
-    $stmt = "SELECT p.person_id,name_ar, sr.role_name, sr.seq_id FROM persons p INNER JOIN research_stuff rs ON p.person_id = rs.person_id INNER JOIN stuff_roles sr ON rs.role_id = sr.seq_id WHERE research_id =" . $_REQUEST['project_id'] . " ORDER BY seq_id";
+    $stmt = "SELECT rs.seq_no,rs.person_id,rs.type, sr.role_name, sr.seq_id
+        FROM research_stuff rs INNER JOIN stuff_roles sr ON rs.role_id = sr.seq_id WHERE research_id =" . $_REQUEST['project_id'] . " ORDER BY seq_id";
+    //echo $stmt;
     $result = $conn->ExecuteNonQuery($stmt);
     $jsonArray = array();
     while ($row = mysql_fetch_array($result)) {
-        $jsonArray[] = array('person_id' => $row['person_id'], 'person_name' => $row['name_ar'] . " -- " . $row['role_name']);
+        if ($row['type'] === 'role_based') {
+            $role_person = $row['role_name'];
+        } else {
+            $role_person = $person->GetPersonName($row['person_id']) . " -- " . $row['role_name'];
+        }
+        $jsonArray[] = array(
+            'research_stuff_id' => $row['seq_no'],
+            'role_person' => $role_person);
     }
     echo json_encode($jsonArray);
 }
