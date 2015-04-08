b646155e910fe10b2ce8c0ceea7da097f4927509
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-08 12:43:10 +0300

    reformat code

---
+++ Data/durationunits.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require_once '../lib/config.php';
+require_once '../lib/mysqlConnection.php';
+
+
+$conn = new MysqlConnect();
+
+
+$stmt = "SELECT seq_id,unit_name FROM duration_units ORDER BY seq_id";
+$result = $conn->ExecuteNonQuery($stmt);
+$jsonArray = array();
+while ($row = mysql_fetch_array($result)) {
+    $jsonArray[] = array('seq_id' => $row['seq_id'], 'unit_name' => $row['unit_name']);
+}
+echo json_encode($jsonArray);
