8827b04ae48d88e2ed24a117b0f9001500f9fe87
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-09 18:31:38 +0300

    reformat code

--- Researchers/searchCoAuthor.php
+++ Researchers/searchCoAuthor.php
@@ -159,7 +159,7 @@ if (isset($_GET['q'])) {
             rowindex = $('#gridCoAuthors').jqxGrid('getselectedrowindex');
             dataRecord = $("#gridCoAuthors").jqxGrid('getrowdata', rowindex);
             person_id = dataRecord['person_id'];
-            alert('person_id::' + person_id);
+//            alert('person_id::' + person_id);
             $('#agreeLetter').jqxFileUpload({uploadUrl: 'inc/fileUpload.php?type=coAuthor_agreement&q=' + '<? echo $project_id; ?>' + '&person_id=' + person_id});
             $('#CVUpload').jqxFileUpload({uploadUrl: 'inc/fileUpload.php?type=resume&q=' + '<? echo $project_id; ?>' + '&person_id=' + person_id});
 
