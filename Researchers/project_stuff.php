278f6f9e1be774bca6d938e96dbb1d7f4c28806d
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-09 17:32:46 +0300

    reformat code

--- Researchers/project_stuff.php
+++ Researchers/project_stuff.php
@@ -90,8 +90,7 @@ $smarty->display('../templates/Loggedin.tpl');
                     {name: 'name_ar'},
                     {name: 'role_name'},
                     {name: 'empCode'},
-                    {name: 'position'},
-                    {name: 'major_Field'}
+                    {name: 'position'}
                 ],
                 id: 'person_id',
                 url: 'ajax/project_stuff_CoI.php?q=<? echo $projectId; ?>'
@@ -115,7 +114,6 @@ $smarty->display('../templates/Loggedin.tpl');
                         {text: 'Emplyoee Id / رقم المنسوب', dataField: 'empCode', width: 200, align: 'center', cellsalign: 'center'},
                         {text: 'Name/ الاسم', dataField: 'name_ar', width: 290, align: 'center', cellsalign: 'right'},
                         {text: 'Title / الدرجة العلمية', dataField: 'position', width: 150, align: 'center', cellsalign: 'right'},
-                        {text: 'Specialization/ التخصص ', dataField: 'major_Field', width: 200, align: 'center', cellsalign: 'right'},
                         {text: 'Delete/حذف', datafield: 'حذف', width: 80, align: 'center', columntype: 'button', cellsrenderer: function () {
                             return '..';
                         }, buttonclick: function (row) {
