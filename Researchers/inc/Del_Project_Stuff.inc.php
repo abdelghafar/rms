a9bac020ead6f8b1661bc5a3bdd283973a25bfdc
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-11 14:37:07 +0300

    reformat code

--- Researchers/inc/Del_Project_Stuff.inc.php
+++ Researchers/inc/Del_Project_Stuff.inc.php
@@ -5,12 +5,10 @@ require_once '../../lib/persons.php';
 require_once '../../lib/research_stuff.php';
 require_once '../../lib/Reseaches.php';
 
-if (isset($_GET['person_id']) && isset($_GET['q'])) {
-    $person_id = filter_input(INPUT_GET, 'person_id', FILTER_VALIDATE_INT);
-    $projectId = filter_input(INPUT_GET, 'q', FILTER_VALIDATE_INT);
+if (isset($_GET['q'])) {
+    $q = filter_input(INPUT_GET, 'q', FILTER_VALIDATE_INT);
     $researchAuthor = new research_stuff();
-    $research = new Reseaches();
-    $res = $researchAuthor->DeleteStuff($projectId, $person_id);
+    $res = $researchAuthor->Delete($q);
     echo $res;
 } else {
     echo 'person_id and rcode are required..';
