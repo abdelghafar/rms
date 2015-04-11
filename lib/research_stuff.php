b508f479310ad63a9ef9ed08b2c727c247661b26
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-11 13:50:52 +0300

    reformat code

--- lib/research_stuff.php
+++ lib/research_stuff.php
@@ -179,7 +179,7 @@ class research_stuff
     {
         $conn = new MysqlConnect();
         $stmt = "SELECT count(*) FROM `stuff_tasks` WHERE `research_stuff_id`= " . $research_stuff_id;
-        echo $stmt . '<br/>';
+//        echo $stmt . '<br/>';
         $result = $conn->ExecuteNonQuery($stmt);
         $count = 0;
         while ($row = mysql_fetch_array($result)) {
@@ -192,6 +192,3 @@ class research_stuff
         //u can not delete
     }
 }
-
-$obj = new research_stuff();
-echo $obj->CanDelete(10001);
