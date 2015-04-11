747c488f0441edaa9d74395d40da5f8e2b02ca8a
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-11 12:49:52 +0300

    reformat code

--- Data/saveOtherPersonal.php
+++ Data/saveOtherPersonal.php
@@ -8,42 +8,35 @@
 
 require_once '../lib/research_stuff.php';
 require_once '../lib/Reseaches.php';
-if (isset($_GET['q']) && isset($_GET['person_id']) && isset($_GET['role_id'])) {
+if (isset($_GET['q']) && isset($_GET['person_id']) && isset($_GET['role_id']) && isset($_GET['file_name']) && isset($_GET['resume_url'])) {
     $research_id = filter_input(INPUT_GET, 'q', FILTER_VALIDATE_INT);
     $person_id = filter_input(INPUT_GET, 'person_id', FILTER_VALIDATE_INT);
     $role_id = filter_input(INPUT_GET, 'role_id', FILTER_VALIDATE_INT);
-
-    if (strlen($person_id == 0)) {
-        echo 'من فضلك اختر الباحث' . '<br/>';
-        return;
-    }
-    if (strlen($research_id == 0)) {
-        echo 'من فضلك اختر المشروع' . '<br/>';
-        return;
-    }
-    if (strlen($role_id == 0)) {
-        echo 'من فضلك اختر الوظيفية' . '<br/>';
-        return;
-    }
+    $agreement_file = filter_input(INPUT_GET, 'file_name', FILTER_SANITIZE_STRING);
+    $resume_file = filter_input(INPUT_GET, 'resume_url', FILTER_SANITIZE_STRING);
 
     $obj = new research_stuff();
     if ($obj->IsExist($research_id, $person_id) == 0) {
         $return_id = $obj->Save($research_id, $person_id, $role_id, research_stuff_categories::$person_based);
-        if (isset($_GET['file_name'])) {
-            $url = filter_input(INPUT_GET, 'file_name', FILTER_SANITIZE_STRING);
-            if (strlen($url)) {
-                $url = "uploads/OtherPersonal_agreement/" . $url;
-                $obj->SetCoAuthor_agreement_url($research_id, $person_id, $url);
-            }
-        }
-        $msg = "";
-        if ($return_id != 0) {
-            return 'Error in insert stmt';
+        if ($agreement_file != 'null' && $resume_file != 'null') {
+            $return_id = $obj->Save($research_id, $person_id, 2, research_stuff_categories::$person_based);
+            $agreement_url = "uploads/" . $research_id . "/" . $agreement_file;
+            $resume_url = "uploads/" . $research_id . "/" . $resume_file;
+
+            $obj->SetCoAuthor_agreement_url($return_id, $agreement_url);
+            $obj->SetCoAuthor_resume_url($return_id, $resume_url);
+            echo 200;
+            return;
         } else {
-            return "sssssssss";
+            echo '<br/>' . 'من فضلك قم بتحميل الموافقة الخطية و السيرة الذاتية للباحث المشارك' . '<br/>';
+            return;
         }
+
     } else {
-        return 'لقد تم تسجبل هذا الشخص من قبل' . '<br/>';
+        echo '<br/>' . 'لقد تم حفظ هذا الشخص من قبل' . '<br/>';
+        return;
     }
+
+} else {
+    echo 'param ar required....';
 }
-return 'لقد تم تسجبل هذا الشخص من قبل';
