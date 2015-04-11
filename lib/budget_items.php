decbbc31be7d680e20170222adb8c9eea81b81dc
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-12 00:23:05 +0300

    reformat code

--- lib/budget_items.php
+++ lib/budget_items.php
@@ -52,6 +52,7 @@ class budget_items
     {
         $conn = new MysqlConnect();
         $stmt = "Select * from " . $this->tableName . " where isSys=0 and parent_item_id=" . $parent_item_id;
+        echo $stmt;
         $rs = $conn->ExecuteNonQuery($stmt);
         return $rs;
     }
