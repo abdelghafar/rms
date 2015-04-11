6faa36875c2a5776e2c8414ce11811eab72f55c1
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-11 15:05:21 +0300

    reformat code

--- Researchers/ajax/project_stuff_consultants.php
+++ Researchers/ajax/project_stuff_consultants.php
@@ -11,12 +11,13 @@ require_once '../../lib/mysqlConnection.php';
 $conn = new MysqlConnect();
 if (isset($_GET['q'])) {
     $projectId = filter_input(INPUT_GET, 'q', FILTER_VALIDATE_INT);
-    $stmt = "SELECT DISTINCT persons.Person_id,persons.Nationality,persons.Position,name_ar,stuff_roles.role_name,persons.Email from persons join research_stuff on research_stuff.person_id = persons.Person_id  join stuff_roles on stuff_roles.seq_id = research_stuff.role_id where research_stuff.research_id=" . $projectId . " and research_stuff.role_id =3 ";
+    $stmt = "SELECT DISTINCT persons.Person_id,research_stuff.seq_no,persons.Nationality,persons.Position,name_ar,stuff_roles.role_name,persons.Email from persons join research_stuff on research_stuff.person_id = persons.Person_id  join stuff_roles on stuff_roles.seq_id = research_stuff.role_id where research_stuff.research_id=" . $projectId . " and research_stuff.role_id =3 ";
     $rs = $conn->ExecuteNonQuery($stmt);
     $r = 0;
     while ($row = mysql_fetch_array($rs, MYSQL_ASSOC)) {
         $result[] = array(
             'person_id' => $row['Person_id'],
+            'seq_no' => $row['seq_no'],
             'name_ar' => $row['name_ar'],
             'role_name' => $row['role_name'],
             'email' => $row['Email'],
