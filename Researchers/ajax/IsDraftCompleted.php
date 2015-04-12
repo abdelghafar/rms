82359dbd14b14664d8b8396bf2e26fd23e92b89e
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-12 10:53:38 +0300

    reformat code

--- Researchers/ajax/IsDraftCompleted.php
+++ Researchers/ajax/IsDraftCompleted.php
@@ -9,5 +9,5 @@ require_once '../../lib/Reseaches.php';
 if (isset($_GET['q'])) {
     $project_id = filter_input(INPUT_GET, 'q', FILTER_VALIDATE_INT);
     $obj = new Reseaches();
-    return $obj->IsDraftCompleted($project_id);
+    echo $obj->IsDraftCompleted($project_id);
 }
