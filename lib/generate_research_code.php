c1cb340b155f9554d800eb9ac08f57a9a5719ba9
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-12 14:07:03 +0300

    reformat code

--- lib/generate_research_code.php
+++ lib/generate_research_code.php
@@ -28,7 +28,7 @@ if (isset($_GET['q'])) {
     $round = $setting->GetCurrRound();
 
     $program_code = $research['program'];
-    $serial = 101;
+    $serial = 100;
     $s = sprintf("%04d", $serial);
     echo $s;
     $stmt = "Select count(*) From researches where research_year=" . $year . " and center_id=" . $tech_id . " and round=" . $round . " and program=" . $program_code . " and isDraft=0";
