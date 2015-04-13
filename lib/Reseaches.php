7c617cc18fd07e280f7db32e6422bca0d5f19cc2
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-13 12:43:50 +0300

    reformat code

--- lib/Reseaches.php
+++ lib/Reseaches.php
@@ -181,7 +181,7 @@ class Reseaches
 
     public function GetResearchDetailsMin($Research_Id)
     {
-        $stmt = "select `researches`.seq_id, `researches`.title_ar,researches.title_en ,`researches`.research_code, `researches`.research_year, technologies.title as `tech_title`,reseach_status.`Status_name`,reseach_status.`Status_Id`,`persons`.`empCode`, persons.name_ar , persons.name_en , tracks.track_name, subtracks.subTrack_name, researches.proposed_duration from `researches` join `reseacher_centers` on `researches`.center_id=`reseacher_centers`.id join `persons` on `persons`.`Person_id` join reseach_status on reseach_status.`Status_Id` = researches.status_id join technologies on technologies.seq_id = researches.center_id join tracks on tracks.track_id = researches.major_field join subtracks on subtracks.seq_id = researches.special_field where `persons`.`Person_id` in (select research_stuff.person_id from research_stuff where research_stuff.role_id = 1 and research_id=$Research_Id) and `researches`.seq_id=$Research_Id";
+        $stmt = "select `researches`.seq_id, `researches`.title_ar,researches.title_en ,`researches`.research_code, `researches`.research_year, `researches`.keywords, technologies.title as `tech_title`,reseach_status.`Status_name`,reseach_status.`Status_Id`,`persons`.`empCode`, persons.name_ar , persons.name_en , tracks.track_name, subtracks.subTrack_name, researches.proposed_duration, project_types.title as 'type_title' , project_types.title_en  as 'type_title_en' from `researches` join `reseacher_centers` on `researches`.center_id=`reseacher_centers`.id join `persons` on `persons`.`Person_id` join reseach_status on reseach_status.`Status_Id` = researches.status_id join technologies on technologies.seq_id = researches.center_id join tracks on tracks.track_id = researches.major_field join subtracks on subtracks.seq_id = researches.special_field join project_types on researches.type_id = project_types.seq_id where `persons`.`Person_id` in (select research_stuff.person_id from research_stuff where research_stuff.role_id = 1 and research_id=$Research_Id) and `researches`.seq_id=$Research_Id";
         $conn = new MysqlConnect();
         $result = $conn->ExecuteNonQuery($stmt);
         return $result;
@@ -663,6 +663,19 @@ class Reseaches
         $stmt = "update researches set `isDraft`=0 where `seq_id`=" . $research_id;
         $rs = $con->ExecuteNonQuery($stmt);
         $rs2 = $this->Lock($research_id);
+        if ($rs == 1 && $rs2 == 1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    
+    public function Research_Submit_Undo($research_id)
+    {
+        $con = new MysqlConnect();
+        $stmt = "update researches set `isDraft`=1 where `seq_id`=" . $research_id;
+        $rs = $con->ExecuteNonQuery($stmt);
+        $rs2 = $this->Unlock($research_id);
         if ($rs == 1 && $rs2 == 1) {
             return 1;
         } else {
