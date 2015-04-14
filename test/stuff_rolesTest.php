2badb730f49c08d54bccc932faff43139ecc82d5
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-14 11:25:21 +0300

    reformat code

--- test/stuff_rolesTest.php
+++ test/stuff_rolesTest.php
@@ -14,7 +14,15 @@ class stuff_rolesTest extends PHPUnit_Framework_TestCase
 
     public function Test___1()
     {
-        echo 'ss';
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack) - 1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
     }
 }
  
