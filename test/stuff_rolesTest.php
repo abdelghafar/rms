718739804bb7eaa4db661cfe229c5e62dab2ae98
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-14 11:28:03 +0300

    reformat code

--- test/stuff_rolesTest.php
+++ test/stuff_rolesTest.php
@@ -7,22 +7,5 @@
  */
 
 require_once '../lib/stuff_roles.php';
-require_once 'PHPUnit/Autoload.php';
 
-class stuff_rolesTest extends PHPUnit_Framework_TestCase
-{
-
-    public function Test___1()
-    {
-        $stack = array();
-        $this->assertEquals(0, count($stack));
-
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack) - 1]);
-        $this->assertEquals(1, count($stack));
-
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
-    }
-}
- 
+echo stuff_roles_system::$Co_Is;
