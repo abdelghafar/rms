ad5898d9f51f11c84346a9d6f40ab1a1d5eefcaf
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-14 13:28:56 +0300

    reformat code

--- Data/saveCoAuthor.php
+++ Data/saveCoAuthor.php
@@ -11,7 +11,7 @@
  */
 
 require_once '../lib/research_stuff.php';
-require_once '../lib/Reseaches.php';
+
 if (isset($_GET['q']) && isset($_GET['person_id']) && isset($_GET['file_name']) && isset($_GET['resume_url'])) {
 
     $research_id = filter_input(INPUT_GET, 'q', FILTER_VALIDATE_INT);
