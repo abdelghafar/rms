15af3208d6ab74375bca742451628a0de3a93701
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-14 11:22:36 +0300

    reformat code

---
+++ test/stuff_rolesTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahmed
+ * Date: 14/04/15
+ * Time: 11:18 ص
+ */
+
+require_once '../lib/stuff_roles.php';
+require_once 'PHPUnit/Autoload.php';
+
+class stuff_rolesTest extends PHPUnit_Framework_TestCase
+{
+
+    public function Test___1()
+    {
+        echo 'ss';
+    }
+}
+ 
