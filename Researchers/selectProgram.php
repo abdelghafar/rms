87d71d64dad64cf01eb31ecca369a3cbdd340141
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-14 15:20:38 +0300

    reformat code

--- Researchers/selectProgram.php
+++ Researchers/selectProgram.php
@@ -1,8 +1,8 @@
 <?
 session_start();
 
-$_SESSION['Authorized'] = 1;
-$_SESSION['person_id'] = 3;
+//$_SESSION['Authorized'] = 1;
+//$_SESSION['person_id'] = 3;
 
 if (isset($_SESSION['Authorized'])) {
     if ($_SESSION['Authorized'] != 1) {
