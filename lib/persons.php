d6b9b2bdb1852c2e2b66e77def1990c000122436
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-22 11:28:37 +0300

    Upload_Finishing_ScholarShip_ChangeList

--- lib/persons.php
+++ lib/persons.php
@@ -110,6 +110,7 @@ class Persons
     {
         $conn = new MysqlConnect();
         $stmt = "update persons set ResumeUrl='" . $url . "' where Person_id=" . $person_id;
+        echo $stmt;
         $rs = $conn->ExecuteNonQuery($stmt);
         return mysql_affected_rows();
     }
