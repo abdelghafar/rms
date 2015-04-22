75c82473dc1cfdbddcd1f8da744f5d5de0dffaa4
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-22 15:23:31 +0300

    Upload_Finishing_ScholarShip_ChangeList

--- Researchers/ajax/checkPDFA.php
+++ Researchers/ajax/checkPDFA.php
@@ -51,7 +51,7 @@ if (isset($_GET['q']) && isset($_GET['type'])) {
             $person_id = $_SESSION['person_id'];
             $p = new Persons();
             $file_name = $p->GetResumeUrl($person_id);
-            echo 'call from ajax/checkPDFA.php==' . $file_name;
+            echo 'call from ajax/checkPDFA.php==' . $p->GetResumeUrl($person_id);
             break;
         }
         case 'finishing_scholarship':
