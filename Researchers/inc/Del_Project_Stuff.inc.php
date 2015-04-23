9219ad8dfb162d591f5d33ad5dc05bccebcb0e85
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-23 10:23:18 +0300

    project_stuff_enhacement

--- Researchers/inc/Del_Project_Stuff.inc.php
+++ Researchers/inc/Del_Project_Stuff.inc.php
@@ -9,12 +9,12 @@ require_once '../../lib/research_stuff.php';
 require_once '../../lib/Reseaches.php';
 
 if (isset($_GET['person_id'])) {
-    $person_id = filter_input(INPUT_GET, 'person_id', FILTER_VALIDATE_INT);
+    $research_stuff_id = filter_input(INPUT_GET, 'research_stuff_id', FILTER_VALIDATE_INT);
     $projectId = $_SESSION['q'];
     $researchAuthor = new research_stuff();
     $research = new Reseaches();
-    // $res = $researchAuthor->DeleteStuff($projectId, $person_id);
-//    echo $res;
+    $res = $researchAuthor->Delete($research_stuff_id);
+    echo $res;
 } else {
     echo 'person_id and rcode are required..';
 }
