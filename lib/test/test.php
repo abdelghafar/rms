f87222cd4ba430fe3ee04c787ef1e4374bf62720
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-23 15:14:04 +0300

    project_stuff_enhacement

--- lib/test/test.php
+++ lib/test/test.php
@@ -2,7 +2,7 @@
 require_once '../research_stuff.php';
 $obj = new research_stuff();
 $role_id = 8;
-$research_id = 1;
+$research_id = 10;
 echo $obj->IsRoleResearchExists($role_id, $research_id);
 
 ?>
