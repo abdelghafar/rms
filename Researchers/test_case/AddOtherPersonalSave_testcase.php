309a4ba3441abb10bac1473da37b2c7d4581a2cb
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-24 22:17:33 +0300

    project_stuff_enhacement

---
+++ Researchers/test_case/AddOtherPersonalSave_testcase.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahmed
+ * Date: 24/04/15
+ * Time: 10:08 م
+ */
+session_start();
+require_once '../../lib/config.php';
+require_once '../../lib/mysqlConnection.php';
+require_once '../../lib/research_stuff.php';
+
+
+$research_stuff_obj = new research_stuff();
+$project_busy_roles = $research_stuff_obj->GetMaxValueRole(1, 5);
+
+$stuff_roles = new stuff_roles();
+$total_roles = $stuff_roles->GetMaxValue(5);
+
+$allowed_project_roles = $total_roles - $project_busy_roles;
+
+echo 'allowed_project_roles=' . $allowed_project_roles;
+
+
