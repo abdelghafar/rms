3df9791624d39c1e552421d808aef0ff1c0e0713
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-25 00:46:19 +0300

    project_stuff_enhacement

--- Researchers/inc/AddOtherPersonalSave.inc.php
+++ Researchers/inc/AddOtherPersonalSave.inc.php
@@ -9,6 +9,8 @@ session_start();
 require_once '../../lib/config.php';
 require_once '../../lib/mysqlConnection.php';
 require_once '../../lib/research_stuff.php';
+require_once '../../lib/stuff_roles.php';
+
 if (isset($_GET['q']) && isset($_GET['p'])) {
     $parent_role_id = filter_input(INPUT_GET, 'q', FILTER_SANITIZE_NUMBER_INT);
     $OtherPersonalCount = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_NUMBER_INT);
@@ -18,25 +20,32 @@ if (isset($_GET['q']) && isset($_GET['p'])) {
         echo '<p class="error">*' . 'من فضلك اختر النوع المشاركة' . '</p>';
         exit();
     }
-    $conn = new MysqlConnect();
-    $stmt = 'SELECT seq_id,role_name FROM stuff_roles where parent_role_id= ' . $parent_role_id . ' and value<=' . $OtherPersonalCount;
-    $rs = $conn->ExecuteNonQuery($stmt);
-    $roles = array();
-    $operation_completed = 0;
-    while ($row = mysql_fetch_array($rs)) {
-        $roles[] = array('seq_id' => $row['seq_id'], 'role_name' => $row['role_name']);
-        $role_id = $row['seq_id'];
-        $obj = new research_stuff();
-        $IsExist = $obj->IsRoleResearchExists($role_id, $project_id);
-        if ($IsExist == 1) {
-            //echo '<p class="error">' . 'لقد تم تسجبل هذا النوع من المشاركين' . '</p>';
-        } else {
-            $obj->Save($project_id, 0, $role_id, research_stuff_categories::$role_based);
-            $operation_completed++;
+
+    $research_stuff_obj = new research_stuff();
+    $project_busy_roles = $research_stuff_obj->GetMaxValueRole($project_id, $parent_role_id);
+
+    $stuff_roles = new stuff_roles();
+    $total_roles = $stuff_roles->GetMaxValue($parent_role_id);
+
+    $allowed_project_slots = $total_roles - $project_busy_roles;
+
+    if ($OtherPersonalCount <= $allowed_project_slots) {
+        $initial_value = $project_busy_roles;
+        $insert_result = 0;
+        for ($i = 0; $i < $OtherPersonalCount; $i++) {
+            $next_role_id = $stuff_roles->GetNextRoleId($parent_role_id, $initial_value);
+            $output = $research_stuff_obj->Save($project_id, 0, $next_role_id, research_stuff_categories::$role_based);
+            $initial_value++;
+            $insert_result++;
         }
+        if ($insert_result == 0) {
+            echo '<p class="error">' . 'لقد حدث خطأ في عملية الادخال برجاء اعادة المحاولة بعد فترة' . '</p>';
+        } else {
+            '<p class="error">' . 'لقد تم حفظ البيانات بنجاح' . '</p>';
+        }
+
+    } else {
+        echo '<p class="error">' . 'لقد بلغت الحد الأقصى' . '</p>';
     }
-    if ($operation_completed == count($roles))
-        echo '<p class="error">' . 'تم حفظ البيانات بنجاح' . '</p>';
-//    else
-//        echo '<p class="error">' . 'خطأ في حفظ البيانات' . '</p>';
+
 }
