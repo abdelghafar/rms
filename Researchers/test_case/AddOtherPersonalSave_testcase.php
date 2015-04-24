8b0182f3107d227632f7938a063565582fce2994
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-25 00:01:19 +0300

    project_stuff_enhacement

--- Researchers/test_case/AddOtherPersonalSave_testcase.php
+++ Researchers/test_case/AddOtherPersonalSave_testcase.php
@@ -11,24 +11,22 @@ require_once '../../lib/mysqlConnection.php';
 require_once '../../lib/research_stuff.php';
 require_once '../../lib/stuff_roles.php';
 
+$parent_role_id = 5;
+$OtherPersonalCount = 2;
+$project_id = 1;
+
 $research_stuff_obj = new research_stuff();
-$project_busy_roles = $research_stuff_obj->GetMaxValueRole(1, 5);
+$project_busy_roles = $research_stuff_obj->GetMaxValueRole($project_id, $parent_role_id);
 
 $stuff_roles = new stuff_roles();
-$total_roles = $stuff_roles->GetMaxValue(5);
+$total_roles = $stuff_roles->GetMaxValue($parent_role_id);
 
-$allowed_project_roles = $total_roles - $project_busy_roles;
+$allowed_project_slots = $total_roles - $project_busy_roles;
 
-//echo 'allowed_project_roles=' . $allowed_project_roles;
-
-$allowed_project_roles = 2;
-$input_count = 3;
-
-if ($input_count <= $allowed_project_roles) {
-    echo 'okay';
+if ($OtherPersonalCount <= $allowed_project_slots) {
+    echo 'u can insert only ' . $OtherPersonalCount;
 } else {
-    echo 'error';
-
+    echo 'u can not insert more than n roles';
 }
 
 
