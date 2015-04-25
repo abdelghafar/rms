78f601ad474921665a9de5766b476895e7df872f
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-25 11:25:56 +0300

    project_stuff_enhacement

--- Researchers/inc/Del_Project_Stuff.inc.php
+++ Researchers/inc/Del_Project_Stuff.inc.php
@@ -4,12 +4,15 @@ session_start();
 require_once '../../lib/persons.php';
 require_once '../../lib/research_stuff.php';
 require_once '../../lib/Reseaches.php';
+require_once '../../lib/project_budget.php';
 
 if (isset($_GET['research_stuff_id'])) {
     $research_stuff_id = filter_input(INPUT_GET, 'research_stuff_id', FILTER_VALIDATE_INT);
     $researchAuthor = new research_stuff();
     $research = new Reseaches();
     $res = $researchAuthor->Delete($research_stuff_id);
+    $project_budget = new project_budget();
+    $project_budget->DeleteByResearchStuffId($research_stuff_id);
     echo $res;
 } else {
     echo 'person_id and rcode are required..';
