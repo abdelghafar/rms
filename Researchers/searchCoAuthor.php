a329918f00e854fb99fbd8e59d1181d40615ef05
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-25 11:51:02 +0300

    project_stuff_enhacement

--- Researchers/searchCoAuthor.php
+++ Researchers/searchCoAuthor.php
@@ -200,8 +200,8 @@ if (isset($_GET['q'])) {
                 </span>
                 <span class="error">*</span>
             </td>
-            <td>
-                <input id="Search" type="text" name="txtSearch"/>
+            <td colspan="2">
+            <input id="Search" type="text" name="txtSearch"/>
                 <input id="searchButton_CoAuthor" value="Search / بحث " type="button"/>
             </td>
         </tr>
