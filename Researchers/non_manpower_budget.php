35227896b8736a9fbfb4b034770bbd57cfcbdad5
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-26 11:14:42 +0300

    project_stuff_enhacement

--- Researchers/non_manpower_budget.php
+++ Researchers/non_manpower_budget.php
@@ -375,6 +375,7 @@ $smarty->display('../templates/header.tpl');
         $.ajax({url: 'ajax/project_total_budegt.php?q=<? echo $projectId ?>', success: function (xdata, textStatus, jqXHR) {
             var project_total = 0;
             project_total = xdata;
+            console.log(project_total);
             $("#project_total").val(project_total);
         }});
     });
