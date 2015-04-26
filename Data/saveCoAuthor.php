10d9ff6458f1154ce9f8fca6111e1e1d42505f71
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-26 14:31:24 +0300

    project_stuff_enhacement

--- Data/saveCoAuthor.php
+++ Data/saveCoAuthor.php
@@ -27,7 +27,7 @@ if (isset($_GET['q']) && isset($_GET['person_id']) && isset($_GET['file_name']) 
             $agreement_url = "uploads/" . $research_id . "/" . $agreement_file;
             $resume_url = "uploads/" . $research_id . "/" . $resume_file;
 
-            $agreement_file = "uploads/" . $research_id . "/" . $_SESSION['tmp_file_name'];
+            $agreement_url = "uploads/" . $research_id . "/" . $_SESSION['tmp_file_name'];
             $obj->SetResearchStuffAgreement($return_id, $agreement_url);
             $obj->SetResearchStuffResume($return_id, $resume_url);
             echo 200;
