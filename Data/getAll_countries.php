15489116192312675abee86b9131b42c98c08269
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-29 22:52:17 +0300

    get all countries as json

--- Data/getAll_countries.php
+++ Data/getAll_countries.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ahmed
- * Date: 29/04/15
- * Time: 09:59 م
- */
+session_start();
 require_once '../lib/countries.php';
 $obj = new countries();
 $obj->GetAll_Json();
