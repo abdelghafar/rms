0aefdc44254246cfa35e2c3347fbe37229057b2d
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-30 00:49:06 +0300

    fix issue #102

--- Researchers/inc/register.inc.php
+++ Researchers/inc/register.inc.php
@@ -8,80 +8,36 @@ require_once '../../lib/Smarty/libs/Smarty.class.php';
 require_once '../../lib/GeneratePasswords.php';
 require_once '../../lib/persons.php';
 
+//ToDo: complete issue #102
 
-$FirstName_ar = "";
-$FatherName_ar = "";
-$GrandName_ar = "";
-$FamilyName_ar = "";
-$FirstName_en = "";
-$FatherName_en = "";
-$GrandName_en = "";
-$FamilyName_en = "";
+$name_ar = "";
+$name_en = "";
 $gender = "";
 $Nationality = "";
-$BirthDate = "";
 $Position = "";
-$major_field = "";
-$special_field = "";
 $university = "";
 $college = "";
 $dept = "";
 $email = "";
 $mobile = "";
-$fax = "";
+$cat_code = 3;
+$ni_image_url = "";
 
 $person = new Persons();
 $isValid = TRUE;
 $response = "";
 
-if (!isset($_POST['FirstName_ar']) || empty($_POST['FirstName_ar'])) {
+if (!isset($_POST['name_ar']) || empty($_POST['name_ar'])) {
     $response .= 'من فضلك ادخل الاسم الأول باللغة العربية' . '<br/>';
     $isValid = FALSE;
 } else
-    $FirstName_ar = mysql_real_escape_string(trim($_POST['FirstName_ar']));
+    $name_ar = mysql_real_escape_string(trim($_POST['name_ar']));
 
-if (!isset($_POST['FatherName_ar']) || empty($_POST['FatherName_ar'])) {
+if (!isset($_POST['name_en']) || empty($_POST['name_en'])) {
     $response .= 'من فضلك أدخل بسم الأب ياللغة العربية' . '<br/>';
     $isValid = FALSE;
 } else
-    $FatherName_ar = mysql_real_escape_string(trim($_POST['FatherName_ar']));
-
-if (!isset($_POST['GrandName_ar']) || empty($_POST['GrandName_ar'])) {
-    $response .= 'من فضلك أدخل اسم الجد باللغة العربية' . '<br/>';
-    $isValid = FALSE;
-} else
-    $GrandName_ar = mysql_real_escape_string(trim($_POST['GrandName_ar']));
-
-if (!isset($_POST['FamilyName_ar']) || empty($_POST['FamilyName_ar'])) {
-    $response .= 'من فضلك ادخل اسم العائلة باللغة العربية' . '<br/>';
-
-    $isValid = FALSE;
-} else
-    $FamilyName_ar = mysql_real_escape_string(trim($_POST['FamilyName_ar']));
-
-if (!isset($_POST['FirstName_en']) || empty($_POST['FirstName_en'])) {
-    $response .= 'من فضلك ادخل الاسم الاول باللغة الانجليزية' . '<br/>';
-    $isValid = FALSE;
-} else
-    $FirstName_en = mysql_real_escape_string(trim($_POST['FirstName_en']));
-
-if (!isset($_POST['FatherName_en']) || empty($_POST['FatherName_en'])) {
-    $response .= 'من فضلك ادخل اسم الأب باللغة الانجليزية' . '<br/>';
-    $isValid = FALSE;
-} else
-    $FatherName_en = mysql_real_escape_string(trim($_POST['FatherName_en']));
-
-if (!isset($_POST['GrandName_en']) || empty($_POST['GrandName_en'])) {
-    $response .= 'من فضلك ادخل اسم الجد باللغة الانجليزية' . '<br/>';
-    $isValid = FALSE;
-} else
-    $GrandName_en = mysql_real_escape_string(trim($_POST['GrandName_en']));
-
-if (!isset($_POST['FamilyName_en']) || empty($_POST['FamilyName_en'])) {
-    $response .= 'من فضلك ادخل اسم العائلة باللغة الانجليزية' . '<br/>';
-    $isValid = FALSE;
-} else
-    $FamilyName_en = mysql_real_escape_string(trim($_POST['FamilyName_en']));
+    $name_en = mysql_real_escape_string(trim($_POST['name_en']));
 
 $gender = $_POST['genderType'];
 
@@ -90,31 +46,12 @@ if ($gender == 'ذكر')
 else
     $gender = 1;
 
-if (!isset($_POST['Nationality']) || empty($_POST['Nationality'])) {
+if (!isset($_POST['selectedCountry']) || empty($_POST['selectedCountry'])) {
     $Nationality = "";
 } else
-    $Nationality = mysql_real_escape_string(trim($_POST['Nationality']));
-
-if (!isset($_POST['BirthDateVal']))
-    $response .= 'من فضلك ادخل تاريخ الميلاد' . '<br/>';
-else {
-    $BirthDate = date_parse($_POST['BirthDateVal']);
-    $BirthDate = $BirthDate['year'] . '-' . $BirthDate['month'] . '-' . $BirthDate['day'];
-}
+    $Nationality = mysql_real_escape_string(trim($_POST['selectedCountry']));
 
 $Position = $_POST['Position'];
-
-if (!isset($_POST['major_field']) || empty($_POST['major_field'])) {
-    $response .= 'من فضلك ادخل التخصص العام' . '<br/>';
-    $isValid = FALSE;
-} else
-    $major_field = mysql_real_escape_string(trim($_POST['major_field']));
-
-if (!isset($_POST['special_field']) || empty($_POST['special_field'])) {
-    $response .= 'من فضلك ادخل التخصص الدقيق' . '<br/>';
-    $isValid = FALSE;
-} else
-    $special_field = mysql_real_escape_string(trim($_POST['special_field']));
 
 if (!isset($_POST['college']) || empty($_POST['college'])) {
     $college = "";
@@ -136,10 +73,6 @@ if (!isset($_POST['mobile']) || empty($_POST['mobile']))
 else
     $mobile = mysql_real_escape_string(trim($_POST['mobile']));
 
-if (!isset($_POST['fax']) || empty($_POST['fax']))
-    $fax = "";
-else
-    $fax = mysql_real_escape_string(trim($_POST['fax']));
 
 $rs = $person->IsExistByEmail($_POST['email']);
 /**
@@ -180,7 +113,7 @@ if ($rs > 0) {
 }
 if ($isValid == TRUE && isset($_POST['key']) && $uploadOk == 1) {
     try {
-        unset($_POST);
+//        unset($_POST);
         if (move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $target_file)) {
             echo "The file " . basename($_FILES["uploadFile"]["name"]) . " has been uploaded.";
             $ni_image_url = basename($_FILES["uploadFile"]["name"]);
@@ -188,7 +121,7 @@ if ($isValid == TRUE && isset($_POST['key']) && $uploadOk == 1) {
             echo "Sorry, there was an error uploading your file.";
         }
 
-        $person_id = $person->Save(0, $FirstName_ar, $FirstName_en, $FatherName_ar, $FatherName_en, $GrandName_ar, $GrandName_en, $FamilyName_ar, $FamilyName_en, $gender, $Nationality, $BirthDate, '', $Position, $major_field, $special_field, $university, $college, $dept, '', '', $email, $mobile, $fax, '', '', '', '', '', '', 3, $ni_image_url);
+        $person_id = $person->Save(0,$name_ar,$name_en,$gender,$Nationality,$Position,$university,$college,$dept,$email,$mobile,$cat_code,$ni_image_url);
 
         echo ' <div class="successbox" style="direction:rtl;width:800px;text-align: right;">';
         echo 'تم حفظ البيانات بنجاح ';
