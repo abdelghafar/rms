0f3f22b1349536e04b2bb99c0b1618b40466412e
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-30 12:18:17 +0300

    fix issue #102 make NI_image optinal

--- Researchers/inc/register.inc.php
+++ Researchers/inc/register.inc.php
@@ -74,28 +74,27 @@ else
     $mobile = mysql_real_escape_string(trim($_POST['mobile']));
 
 
-if(!isset($_FILES['uploadFile']['name']) || empty($_FILES['uploadFile']['name']))
-{
-    $uploadOk =1;
-    echo '<p class="error">'.'من فضلك قم بتحميل صورة اثبات الهوية'.'</p>';
+if (isset($_FILES['uploadFile']['name']) && count($_FILES['uploadFile']['name']) > 0) {
+    $uploadOk = 1;
+    echo '<p class="error">' . 'من فضلك قم بتحميل صورة اثبات الهوية' . '</p>';
     if (file_exists($target_file)) {
-        echo "<pre>"."Sorry, file already exists.".'</pre>';
+        echo "<pre>" . "Sorry, file already exists." . '</pre>';
         $uploadOk = 0;
     }
-    if ($_FILES["fileToUpload"]["size"] > 500000 || $_FILES["fileToUpload"]["size"]==0) {
-        echo "<pre>"."Sorry, your file is too large.".'</pre>';
+    if ($_FILES["fileToUpload"]["size"] > 500000 || $_FILES["fileToUpload"]["size"] == 0) {
+        echo "<pre>" . "Sorry, your file is too large." . '</pre>';
         $uploadOk = 0;
     }
 // Allow certain file formats
     if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
         && $imageFileType != "gif" && $imageFileType != "pdf"
     ) {
-        echo "<pre>"."Sorry, only JPG, JPEG, PNG,GIF and PDF files are allowed.".'</pre>';
+        echo "<pre>" . "Sorry, only JPG, JPEG, PNG,GIF and PDF files are allowed." . '</pre>';
         $uploadOk = 0;
     }
 // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        echo "<pre>"."Sorry, your file was not uploaded."."<pre>";
+        echo "<pre>" . "Sorry, your file was not uploaded." . "<pre>";
 // if everything is ok, try to upload file
     }
 
@@ -111,12 +110,11 @@ $target_file = $target_dir . basename($_FILES["uploadFile"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
-if($uploadOk==1)
-{
+if ($uploadOk == 1) {
     if (move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $target_file)) {
 //        echo "The file " . basename($_FILES["uploadFile"]["name"]) . " has been uploaded.";
-        $file_name = pathinfo(basename($_FILES["uploadFile"]["name"]),PATHINFO_FILENAME).pathinfo(basename($_FILES["uploadFile"]["name"]),PATHINFO_EXTENSION);
-        echo 'file name is = '.$file_name;
+        $file_name = pathinfo(basename($_FILES["uploadFile"]["name"]), PATHINFO_FILENAME) . pathinfo(basename($_FILES["uploadFile"]["name"]), PATHINFO_EXTENSION);
+        echo 'file name is = ' . $file_name;
         $ni_image_url = basename($_FILES["uploadFile"]["name"]);
     } else {
         echo "Sorry, there was an error uploading your file.";
@@ -131,8 +129,7 @@ if ($isValid == TRUE && isset($_POST['key'])) {
     try {
 //        unset($_POST);
 
-        $person_id = $person->Save(0,$name_ar,$name_en,$gender,$Nationality,$Position,$university,$college,$dept,$email,$mobile,$cat_code,$ni_image_url);
-
+        $person_id = $person->Save(0, $name_ar, $name_en, $gender, $Nationality, $Position, $university, $college, $dept, $email, $mobile, $cat_code, $ni_image_url);
 
 
         echo ' <div class="successbox" style="direction:rtl;width:800px;text-align: right;">';
