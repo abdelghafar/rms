aed7411596844a9066dada2ecc48f4bdb27aa9d2
Ahmed I. Sharaf <ahmed.sharaf.84@gmail.com>
2015-04-30 13:53:58 +0300

    fix issue #102 try pathinfo

--- Researchers/inc/register.inc.php
+++ Researchers/inc/register.inc.php
@@ -81,6 +81,7 @@ if (!empty($_FILES['uploadFile']['name'])) {
     $target_dir = "../../uploads/";
 //    $target_file = $target_dir . basename($_FILES["uploadFile"]["name"]);
     $target_dir = $target_dir.basename(pathinfo($_FILES["uploadFile"]["name"],PATHINFO_FILENAME).'.'.pathinfo($_FILES["uploadFile"]["name"],PATHINFO_EXTENSION));
+    echo 'target_dir='.$target_dir;
     $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
     if (file_exists($target_file)) {
         echo "<pre>" . "Sorry, file already exists." . '</pre>';
